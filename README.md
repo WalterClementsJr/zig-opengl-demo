02374f92991c66532ec31aa909d3420f69841540
Walker <61227405+WalterClementsJr@users.noreply.github.com>
2025-12-13 11:42:00 +0700

    init project build kit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Installation
+
+Dependencies:
+
+- OpenGL bindings
+
+```sh
+zig fetch --save https://github.com/ziglibs/zgl/archive/6ee54c287f3c4f176bd05a92746828bb791a1ad0.tar.gz
+```
+
+- GLFW: glfw-devel / libglfw, depend on your package manager
+
+Then test with `zig build`.
+
---
+++ build.zig
@@ -0,0 +1,57 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+    const mod = b.addModule("zig_opengl_cube", .{
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+    });
+    const exe = b.addExecutable(.{
+        .name = "zig_opengl_cube",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+            .imports = &.{
+                .{ .name = "zig_opengl_cube", .module = mod },
+            },
+        }),
+    });
+    exe.linkSystemLibrary("glfw");
+
+    // zig opengl binding
+    const zgl = b.dependency("zgl", .{
+        .target = target,
+        .optimize = optimize,
+    });
+    exe.root_module.addImport("zgl", zgl.module("zgl"));
+
+    b.installArtifact(exe);
+
+    const run_step = b.step("run", "Run the app");
+    const run_cmd = b.addRunArtifact(exe);
+    run_step.dependOn(&run_cmd.step);
+
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const mod_tests = b.addTest(.{
+        .root_module = mod,
+    });
+
+    const run_mod_tests = b.addRunArtifact(mod_tests);
+
+    const exe_tests = b.addTest(.{
+        .root_module = exe.root_module,
+    });
+
+    const run_exe_tests = b.addRunArtifact(exe_tests);
+
+    const test_step = b.step("test", "Run tests");
+    test_step.dependOn(&run_mod_tests.step);
+    test_step.dependOn(&run_exe_tests.step);
+}
